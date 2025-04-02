fa98daba32530d9e738267fbd88826604eb76364
kave.haj <kave.haj@snapp.cab>
2025-04-03 00:05:12 +0330

    some changes

--- api/user_handler.go
+++ api/user_handler.go
@@ -2,20 +2,40 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/kave08/hotel-reservation/db"
 	"github.com/kave08/hotel-reservation/types"
 )
 
-func HandleGetUsers(c fiber.Ctx) error {
+type UserHanlder struct {
+	UserStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHanlder {
+	return &UserHanlder{
+		UserStore: userStore,
+	}
+}
+
+func (h *UserHanlder) HandleGetUser(c fiber.Ctx) error {
+	id := c.Params("id")
+	user, err := h.UserStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		return c.JSON(map[string]string{
+			"error": fiber.ErrNotFound.Message,
+		})
+	}
+
+	return c.JSON(map[string]string{
+		"first_name": user.FirstName,
+		"last_name":  user.LastName,
+	})
+}
+
+func (h *UserHanlder) HandleGetUsers(c fiber.Ctx) error {
 	u := types.User{
 		ID:        "1",
 		FirstName: "kave",
 		LastName:  "hudj",
 	}
 	return c.JSON(u)
-}
-
-func HandleGetUser(c fiber.Ctx) error {
-	return c.JSON(map[string]string{
-		"user": "kaves",
-	})
 }
--- main.go
+++ main.go
@@ -7,51 +7,32 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/kave08/hotel-reservation/api"
-	"github.com/kave08/hotel-reservation/types"
-	"go.mongodb.org/mongo-driver/bson"
+	"github.com/kave08/hotel-reservation/db"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
 	dbURI   = "mongodb://localhost:27017"
-	dbName  = "hotel-reservation"
-	userCol = "users"
+	
 )
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
+	flag.Parse()
+
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	user := types.User{
-		FirstName: "kave",
-		LastName:  "hudj",
-	}
 
-	coll := client.Database(dbName).Collection(userCol)
-	_, err = coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	var kave types.User
-
-	err = coll.FindOne(ctx, bson.M{}).Decode(&kave)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
-	flag.Parse()
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	err = app.Listen(*listenAddr)
 	if err != nil {
