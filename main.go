f30f333a090a268961c659e0cc7077d003001fa0
kave08 <kave.mailbox@gmail.com>
2025-06-28 23:29:21 +0330

    update mongodb connection string

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
 	"github.com/kave08/hotel-reservation/db"
 	"github.com/kave08/hotel-reservation/handler"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const (
-	dbURI = "mongodb://localhost:27017"
-)
 
 var config = fiber.Config{
 	ErrorHandler: func(c fiber.Ctx, err error) error {
@@ -28,10 +28,36 @@ func main() {
 	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
 	flag.Parse()
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
-	if err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Failed to load .env file:", err)
 	}
+
+	username := os.Getenv("MONGO_USERNAME")
+	password := os.Getenv("MONGO_PASSWORD")
+	dbName := os.Getenv("MONGO_DATABASE")
+	dbURI := "mongodb://" + username + ":" + password + "@localhost:27017/" + dbName + "?authSource=admin"
+
+	// Connect to MongoDB
+	clientOptions := options.Client().ApplyURI(dbURI).SetConnectTimeout(10 * time.Second)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Fatal("Failed to disconnect from MongoDB:", err)
+		}
+	}()
+
+	// Ping the database
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
+	}
+
+	log.Println("Successfully connected to MongoDB!")
 
 	userHandler := handler.NewUserHandler(db.NewMongoUserStore(client))
 
